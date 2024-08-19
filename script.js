3594576fb7508976754c02e672e16eadd78b3194
Kunal Kurve <165116100+KunalKurve@users.noreply.github.com>
2024-08-19 19:42:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('resume-btn').addEventListener('click', function() {
+    window.open('resume.pdf', '_blank');
+});
+
+document.getElementById('github-btn').addEventListener('click', function() {
+    window.open('https://github.com/KunalKurve', '_blank');
+});
